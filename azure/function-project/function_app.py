f9bcc14ec4c5b2b9280c31faeea47aa2cb2f929a
Tino <tino.viena@yahoo.com>
2024-01-27 19:44:38 +0100

    now also supports reading

--- azure/function-project/function_app.py
+++ azure/function-project/function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 from azure.core.exceptions import ResourceExistsError
-from azure.data.tables import TableServiceClient
+from azure.data.tables import TableServiceClient, TableClient
 from azure.core.credentials import AzureNamedKeyCredential
 import logging
 import json
@@ -20,53 +20,92 @@ def mk_acc_http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
     response = {}
-    if not name:
+    try:
+        req_body = json.loads(req.get_body())
+        sec = req_body.get("secret")
+        if (sec == None or sec == '' or sec != "c0fa0758-b954-11ee-9a88-3baa8ec87383"):
+            return BuggerOffResponse
+        
+        with TableServiceClient(endpoint="https://ml015707162299.table.core.windows.net", credential=credential) as table_service_client:
+            properties = table_service_client.get_service_properties()
+            table_client = table_service_client.get_table_client(table_name="Accruals")
+            recs = req_body.get("records")
+            if (recs == None):
+                logging.error("FAIL - there is no 'records' element on root level")
+                return BuggerOffResponse
+            for my_entity in recs :
+                if (not isValid(my_entity)):
+                    #return BuggerOffResponse
+                    continue
+                my_entity["PartitionKey"] = "Accruals"
+                my_entity["RowKey"] = sha1_hash(my_entity)
+                logging.info(req_body)
+                try:
+                    entity = table_client.create_entity(entity=my_entity)
+                    logging.info(f"SUCCESS - {my_entity['RowKey']} stored: {entity}")
+                    response["id"] = my_entity["RowKey"]
+                    response["message"] = str(entity)
+                except ResourceExistsError as e:
+                    logging.error(f'Entity with id {my_entity["RowKey"]} already exists {type(e).__name__}: {e}')
+                    logging.info(f"Due to a ResourceExistsError nothing was stored of this: {my_entity}")
+                except Exception as e:
+                    logging.error(f'Storing {my_entity["RowKey"]} resulted in an error of type {type(e).__name__}: {e}')
+                    logging.info(f"Due to a {type(e).__name__} nothing was stored of this: {my_entity}")
+
+    except ValueError:
+        pass
+
+    return func.HttpResponse(
+            json.dumps(response),
+            status_code=200
+    )
+
+@app.route(route="mk_acc_http_trigger_read")
+def mk_acc_http_trigger_read(req: func.HttpRequest) -> func.HttpResponse:
+    credential = AzureNamedKeyCredential(os.environ["AZ_TABLE_ACCOUNT_NAME"], os.environ["AZURE_TABLE_ACCOUNT_KEY"])
+
+    logging.info('Python HTTP trigger read function processed a request.')
+
+    if (req.method == 'POST'):
+        response = {}
         try:
             req_body = json.loads(req.get_body())
             sec = req_body.get("secret")
             if (sec == None or sec == '' or sec != "c0fa0758-b954-11ee-9a88-3baa8ec87383"):
                 return BuggerOffResponse
-            
-            with TableServiceClient(endpoint="https://ml015707162299.table.core.windows.net", credential=credential) as table_service_client:
-                properties = table_service_client.get_service_properties()
-                table_client = table_service_client.get_table_client(table_name="Accruals")
-                recs = req_body.get("records")
-                if (recs == None):
-                    logging.error("FAIL - there is no 'records' element on root level")
-                    return BuggerOffResponse
-                for my_entity in recs :
-                    if (not isValid(my_entity)):
-                        #return BuggerOffResponse
-                        continue
-                    my_entity["PartitionKey"] = "Accruals"
-                    my_entity["RowKey"] = sha1_hash(my_entity)
-                    logging.info(req_body)
-                    try:
-                        entity = table_client.create_entity(entity=my_entity)
-                        logging.info(f"SUCCESS - {my_entity['RowKey']} stored: {entity}")
-                        response["id"] = my_entity["RowKey"]
-                        response["message"] = str(entity)
-                    except ResourceExistsError as e:
-                        logging.error(f'Entity with id {my_entity["RowKey"]} already exists {type(e).__name__}: {e}')
-                        logging.info(f"Due to a ResourceExistsError nothing was stored of this: {my_entity}")
-                    except Exception as e:
-                        logging.error(f'Storing {my_entity["RowKey"]} resulted in an error of type {type(e).__name__}: {e}')
-                        logging.info(f"Due to a {type(e).__name__} nothing was stored of this: {my_entity}")
+            acc = req_body.get("account")
+            if (acc == None):
+                logging.error("FAIL - there is no 'records' element on root level")
+                return BuggerOffResponse
+
+            with TableClient(table_name="Accruals", endpoint="https://ml015707162299.table.core.windows.net", credential=credential) as table_client:
+                #table_client = table_service_client.get_table_client(table_name="Accruals")
+                try:
+                    parameters = {
+                        "account": acc,
+                    }
+                    query_filter = "account eq @account"
+                    records = table_client.query_entities(query_filter, parameters=parameters)
+                    total = 0
+                    total = sum(d['amountEuro'] for d in records) 
+
+                    logging.info(f"SUCCESS - {records}")
+                    response["total"] = total
+                    response["message"] = "we are happy"
+                except Exception as e:
+                    logging.error(f'resulted in an error of type {type(e).__name__}: {e}')
+                    logging.info(f"Due to a {type(e).__name__} nothing was retrieved from this: {acc}")
 
         except ValueError:
             pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             json.dumps(response),
-             status_code=200
-        )
+    elif(req.method == 'GET'):
+        return BuggerOffResponse
+    
+    return func.HttpResponse(
+            json.dumps(response),
+            status_code=200
+    )
 
 
 def isValid(record):
--- azure/test/post-request.py
+++ azure/test/post-request.py
@@ -1,26 +1,55 @@
+import sys
 import requests
 import json
 
-url = 'http://localhost:7071/api/mk_acc_http_trigger'
-data = {    
-    "secret": "c0fa0758-b954-11ee-9a88-3baa8ec87383",
-    "name": "Sweet Jane",
-    "records": [
-        {
-            "day": "2022-01-01",
-            "amountEuro": 100.2,
-            "description": "Groceries",
-            "account": "essen",
-        },
-        {
-            "day": "2022-01-02",
-            "amountEuro": 50.0,
-            "description": "Gasoline",
-            "CUNT": "",
-        }
-    ]
-}
+base_url = 'http://localhost:7071/api'
 
-response = requests.post(url, json=json.dumps(data))
-print(response.status_code)
-print(response.text)
+def test_write():
+    url = base_url + '/mk_acc_http_trigger'
+    data = {    
+        "secret": "c0fa0758-b954-11ee-9a88-3baa8ec87383",
+        "name": "Sweet Jane",
+        "records": [
+            {
+                "day": "2022-01-01",
+                "amountEuro": 100.2,
+                "description": "Groceries",
+                "account": "essen",
+            },
+            {
+                "day": "2022-01-02",
+                "amountEuro": 50.0,
+                "description": "Gasoline",
+                "CUNT": "",
+            }
+        ]
+    }
+
+    response = requests.post(url, json=data)
+    print(response.status_code)
+    print(response.text)
+
+def test_read():
+    url = base_url + '/mk_acc_http_trigger_read'
+    data = {
+        "secret": "c0fa0758-b954-11ee-9a88-3baa8ec87383", 
+        'account': 'essen',
+    }
+    response = requests.post(url, json=data)
+    print(response.status_code)
+    print(response.text)
+
+if (__name__ == '__main__'):
+    arg = sys.argv[1]
+    if (arg == 'write'):
+        test_write()
+    elif (arg == 'read'):
+        test_read()
+    else:
+        method_name = sys.argv[1]
+        try:
+            method = getattr(sys.modules[__name__], method_name)
+        except AttributeError:
+            print(f"Error: {method_name} is not a valid method name.")
+            sys.exit(1)
+        method()
